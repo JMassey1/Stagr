5b23f21562a4a13121b4d649ee3bb9f649b27c0a
Jordan Massey <j.massey2278@gmail.com>
2025-06-19 18:12:47 -0500

    main App now uses TabView

--- Stagr/StagrApp.swift
+++ Stagr/StagrApp.swift
@@ -2,9 +2,27 @@ import SwiftUI
 
 @main
 struct StagrApp: App {
+    @StateObject private var viewModel = AllShowsViewModel()
+  
     var body: some Scene {
         WindowGroup {
-            AllShowsView()
+          TabView {
+            NavigationView {
+              ShowListView(viewModel: viewModel)
+                .navigationTitle("All Shows")
+            }
+            .tabItem {
+              Label("All Shows", systemImage: "list.bullet")
+            }
+            
+            NavigationView {
+              ShowListView(viewModel: viewModel, shows: viewModel.interestedShows)
+                .navigationTitle("My Shows")
+            }
+            .tabItem {
+              Label("My Shows", systemImage: "star.fill")
+            }
+          }
         }
     }
 }
