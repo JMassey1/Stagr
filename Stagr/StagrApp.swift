98c8e430d9c5d502208206bec0ec44f85da13a68
Jordan Massey <j.massey2278@gmail.com>
2025-06-19 15:31:23 -0500

    Initial Commit

---
+++ Stagr/StagrApp.swift
@@ -0,0 +1,17 @@
+//
+//  StagrApp.swift
+//  Stagr
+//
+//  Created by Jordan Massey on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct StagrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
