c6fab8acb58dfd0ae36826d0942a6c75fbb75491
Jordan Massey <j.massey2278@gmail.com>
2025-06-19 16:05:32 -0500

    basic viewModel with mock data and a toggle function

--- Stagr/Stagr/AllShows/AllShowsViewModel.swift
+++ Stagr/Stagr/AllShows/AllShowsViewModel.swift
@@ -1,7 +1,22 @@
-//
-//  AllShowsViewModel.swift
-//  Stagr
-//
-//  Created by Jordan Massey on 6/19/25.
-//
+import Foundation
 
+final class AllShowsViewModel: ObservableObject {
+  @Published var shows: [Show] = []
+  
+  init() {
+    loadMockData()
+  }
+  
+  func toggleInterested(for show: Show) {
+    guard let index = shows.firstIndex(of: show) else { return }
+    shows[index].isInterested.toggle()
+  }
+  
+  private func loadMockData() {
+    shows = [
+      Show(id: UUID(), artist: "ODESZA", stage: "What Stage", day: "Friday", time: "22:00", isInterested: false),
+      Show(id: UUID(), artist: "Fred again..", stage: "That Tent", day: "Thursday", time: "12:00", isInterested: true),
+      Show(id: UUID(), artist: "2hollis", stage: "This Tent", day: "Saturday", time: "19:00", isInterested: true)
+    ]
+  }
+}
