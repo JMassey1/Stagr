6abdbf00faccf535f57c45d4291dae23e6953eb2
Jordan Massey <j.massey2278@gmail.com>
2025-06-24 23:52:53 -0500

    added intermediate view to (kinda) prevent view heirarchy crashes

--- Stagr/StagrApp.swift
+++ Stagr/StagrApp.swift
@@ -4,12 +4,16 @@ import SwiftData
 @main
 struct StagrApp: App {
   
-    var body: some Scene {
-        WindowGroup {
-          FestivalListView()
-        }
-        .modelContainer(for: [Festival.self, Show.self]) { val in
-            print(val)
-        }
+  var body: some Scene {
+    WindowGroup {
+      RootView()
     }
+    .modelContainer(for: [Festival.self, Show.self])
+  }
+}
+
+struct RootView: View {
+  var body: some View {
+    FestivalListView()
+  }
 }
