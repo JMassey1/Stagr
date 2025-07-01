b0e0552e828e0fbf8784e1822a279107a7134538
Jordan Massey <j.massey2278@gmail.com>
2025-07-01 00:57:17 -0500

    removed custom swiftlint, fixed func name

--- StagrTests/Views/FestivalList/AddFestivalViewTest.swift
+++ StagrTests/Views/FestivalList/AddFestivalViewTest.swift
@@ -6,13 +6,13 @@ import SwiftData
 @Suite("AddFestivalView Tests")
 struct AddFestivalViewTest {
   let testHelper: TestHelper
-  
+
   init() throws {
     testHelper = try TestHelper()
   }
-  
+
   @Test("Adding Festival")
-  func AddingFestival() throws {
+  func addingFestival() throws {
     let festival = Festival(
       id: UUID(),
       name: "Test Festival",
@@ -20,9 +20,9 @@ struct AddFestivalViewTest {
       endDate: Calendar.current.date(byAdding: .day, value: 3, to: Date()) ?? Date()
     )
     testHelper.modelContext.insert(festival)
-    
+
     let fetchDescriptor = FetchDescriptor<Festival>()
-    
+
     let festivals = try testHelper.modelContext.fetch(fetchDescriptor)
     #expect(festivals.count == 1)
     #expect(festivals[0] == festival)
